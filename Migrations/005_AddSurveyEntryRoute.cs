555b5ce31a8d80890e7e5b725a9f6942af1906e5
Daniel Hazelbaker <daniel@blueboxmoon.com>
2018-02-12 12:35:05 -0800

    + Added migrations to fix survey entry route and result detail page location.

---
+++ Migrations/005_AddSurveyEntryRoute.cs
@@ -0,0 +1,16 @@
+﻿using Rock.Plugin;
+
+namespace com.shepherdchurch.SurveySystem.Migrations
+{
+    public class AddSurveyEntryRoute : Migration
+    {
+        public override void Up()
+        {
+            RockMigrationHelper.AddPageRoute( SystemGuid.Page.SURVEY_ENTRY, "surveyentry/{SurveyId}" );
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Migrations/006_MoveSurveyResultDetail.cs
@@ -0,0 +1,18 @@
+﻿using Rock.Plugin;
+
+namespace com.shepherdchurch.SurveySystem.Migrations
+{
+    [MigrationNumber(6, "1.6.3")]
+    public class MoveSurveyResultDetails : Migration
+    {
+        public override void Up()
+        {
+            RockMigrationHelper.MovePage( SystemGuid.Page.SURVEY_RESULT_DETAILS, SystemGuid.Page.SURVEY_RESULTS );
+        }
+
+        public override void Down()
+        {
+            RockMigrationHelper.MovePage( SystemGuid.Page.SURVEY_RESULT_DETAILS, SystemGuid.Page.SURVEY_RESULT_DETAILS );
+        }
+    }
+}
