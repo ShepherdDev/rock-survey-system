3225e529abc6976ccc4c6c43ed5bdb9a8f2734b4
Daniel Hazelbaker <daniel@blueboxmoon.com>
2020-08-25 16:02:10 -0700

    + Added support for Pre and PostHtml in survey questions.

---
+++ Migrations/011_EnablePrePostHtml.cs
@@ -0,0 +1,18 @@
+﻿using Rock.Plugin;
+
+namespace com.shepherdchurch.SurveySystem.Migrations
+{
+    [MigrationNumber( 11, "1.10.2" )]
+    public class EnablePrePostHtml : Migration
+    {
+        public override void Up()
+        {
+            Sql( $"UPDATE [EntityType] SET [AttributesSupportPrePostHtml] = 1 WHERE [Guid] = '{SystemGuid.EntityType.SURVEY_RESULT}'" );
+        }
+
+        public override void Down()
+        {
+            Sql( $"UPDATE [EntityType] SET [AttributesSupportPrePostHtml] = 0 WHERE [Guid] = '{SystemGuid.EntityType.SURVEY_RESULT}'" );
+        }
+    }
+}
